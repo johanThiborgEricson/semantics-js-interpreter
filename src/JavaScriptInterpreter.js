ad5375dc92b42c50d13eb91cfa7f22a843b22bba
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-09-01 18:35:44 +0200

    Wrote down the current grammar.

--- src/JavaScriptInterpreter.js
+++ src/JavaScriptInterpreter.js
@@ -112,13 +112,6 @@ function JavaScriptInterpreter() {
     this.executionContext.variables[bindingIdentifier] = initialiserOpt;
   });
   
-  j.initialiser = f.group(/=/, "assignmentExpression", 
-  function(assignmentExpression) {
-    return assignmentExpression;
-  });
-  
-  j.initialiserOpt = f.opt("initialiser", undefined);
-  
   j.returnStatement = f.group(/return /, "expression", /;/, 
   function(expression) {
     return expression;
