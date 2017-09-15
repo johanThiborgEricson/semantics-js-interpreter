415be3109aa618cfbe59178f368201cb964e265e
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-09-15 11:20:23 +0200

    There are four simple literals: undefined, null, true and false.

---
+++ spec/literal.js
@@ -0,0 +1,21 @@
+describe("The literal", function() {
+  
+  var interpreter = new JavaScriptInterpreter();
+  
+  it("undefined is undefined", function() {
+    expect(interpreter.program("return undefined;")).toBe(undefined);
+  });
+  
+  it("null is null", function() {
+    expect(interpreter.program("return null;")).toBe(null);
+  });
+  
+  it("true is true", function() {
+    expect(interpreter.program("return true;")).toBe(true);
+  });
+  
+  it("false is false", function() {
+    expect(interpreter.program("return false;")).toBe(false);
+  });
+  
+});
