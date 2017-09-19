f127b1fcfa57012e0c3220841b640d4e80e4425f
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-09-20 00:11:39 +0200

    I will not support left hand sides inside parentheses for a while.

--- spec/leftHandSideExpression.js
+++ spec/leftHandSideExpression.js
@@ -68,19 +68,4 @@ describe("A left hand side expression", function() {
     ).toBe(1);
   });
   
-  it("may be inside parentheses", function() {
-    expect(interpreter.program(
-      "var a;" +
-      "(a)=1;" +
-      "return a;")).toBe(1);
-  });
-  
-  it("may be prepended by a side effect expression", function() {
-    expect(interpreter.program(
-      "var f=function(){};" +
-      "var a;" +
-      "(f(),a)=1;" +
-      "return a;")).toBe(1);
-  });
-  
 });
